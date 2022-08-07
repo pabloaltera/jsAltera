aab20060a63e7ac9615901d541c02c2a0d810c7e
Pablo Altera <pabloaltera@gmail.com>
2022-08-07 20:31:34 -0300

    2da entrega

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const formAltas = document.querySelector("#formularioAlta")
 const divIngreso = document.querySelector(".ingreso")
 const divAltas = document.querySelector(".altas")
 const divPadron = document.querySelector(".padron")
+const divBotonera = document.querySelector(".botonera")
 const usuario = document.querySelector("#userId")
 const contrasenia = document.querySelector("#inputPassword")
 const fdni = document.querySelector("#inputDni")
@@ -13,41 +14,40 @@ const fdireccion = document.querySelector("#inputDireccion")
 const fciudad = document.querySelector("#inputCiudad")
 const fprovincia = document.querySelector("#inputProvincia")
 const fcodPostal = document.querySelector("#inputCodPostal")
+const logOut = document.querySelector("#logOut")
+const borrar = document.querySelector("#borrar")
+
+
+let padron = []
+const obtenerPadrondDelLS = localStorage.getItem("padron")
+const padronParseado = JSON.parse(obtenerPadrondDelLS) ?? []
+padron = (padronParseado)
+
+function subirPadronLS() {
+    padronJSON = JSON.stringify(padron)
+    localStorage.setItem("padron", padronJSON)
+}
 
 
 function revisarIngreso(usuario, contrasenia) {
-    if (usuario !== "admin" || contrasenia !== "1234"){
-        alert ("Usted ha ingresado credenciales incorrectas, por favor, vuelva a intentarlo")
+    if (usuario !== "admin" || contrasenia !== "1234") {
+        alert("Usted ha ingresado credenciales incorrectas, por favor, vuelva a intentarlo")
     }
     else {
         divIngreso.style.display = "none"
         divAltas.style.display = "flex"
         divPadron.style.display = "flex"
         divPadron.style.flexDirection = "column"
-        console.log(divPadron)
+        divBotonera.style.display = "flex"
     }
 }
 
 formIngreso.onsubmit = (event) => {
     event.preventDefault()
-    revisarIngreso(usuario.value,contrasenia.value)
+    revisarIngreso(usuario.value, contrasenia.value)
 }
 
-class Afiliado {
-    constructor(dni, nombre, apellido, domicilio, codPostal, ciudad, provincia,){
-        this.dni = dni
-        this.nombre = nombre
-        this.apellido = apellido
-        this.domicilio = domicilio
-        this.codPostal = codPostal
-        this.ciudad = ciudad
-        this.provincia = provincia
-    }
-}
-
-const padron = [] 
-
-function nuevoAfiliado(fdni,fnombre,fapellido,fdireccion,fcodPostal,fciudad,fprovincia){
+function nuevoAfiliado(fdni, fnombre, fapellido, fdireccion, fcodPostal, fciudad, fprovincia) {
     dni = fdni
     nombre = fnombre
     apellido = fapellido
@@ -55,45 +55,60 @@ function nuevoAfiliado(fdni,fnombre,fapellido,fdireccion,fcodPostal,fciudad,fpro
     codPostal = fcodPostal
     ciudad = fciudad
     provincia = fprovincia
-    
+
     if (padron.some((Afiliado) => {
         return Afiliado.dni === dni
     })) {
         alert(`El DNI  ${dni} ingresado ya figura en la base de datos!!`)
     } else {
-        afiliado = new Afiliado (dni, nombre, apellido, domicilio, codPostal, ciudad, provincia)
-
-        padron.push(afiliado)
+        padron.push({ dni, nombre, apellido, domicilio, codPostal, ciudad, provincia })
     }
 
-    console.log(padron)
+    subirPadronLS()
 
-    
+
 }
+
+function mostrarPadron() {
+    const tarjetasHtml = padron.reduce((acc, elemento, i) => {
+
+        return acc = acc + `    
+            <tr class="tabla"> 
+                <td>${elemento.apellido} ${elemento.nombre}</td>
+                <td>DNI: ${elemento.dni}</td>
+                <td>Domicilio: ${elemento.domicilio}</td>
+                <td>Cod. Postal: ${elemento.codPostal}</td>
+                <td>Ciudad: ${elemento.ciudad}</td>
+                <td>Privincia: ${elemento.provincia}</td>
+            </tr>
+        `
+    }, "<table> <tr> <th>APELLIDO Y NOMBRE</th>  <th>DNI</th> <th>DOMICILIO</th> <th>COD POSTAL</th> <th>CIUDAD</th> <th>PROVINCIA</th>  </tr>")
+
+    const contenedorAfiliados = document.querySelector(".contenedorAfiliados")
+
+    contenedorAfiliados.innerHTML = (tarjetasHtml + "</table>")
+}
+
+mostrarPadron()
 
 formAltas.onsubmit = (event) => {
     event.preventDefault()
-    console.log(fdni.value,fnombre.value,fapellido.value,fdireccion.value,fcodPostal.value,fciudad.value,fprovincia.value)
-    nuevoAfiliado(fdni.value,fnombre.value,fapellido.value,fdireccion.value,fcodPostal.value,fciudad.value,fprovincia.value)
+    nuevoAfiliado(fdni.value, fnombre.value, fapellido.value, fdireccion.value, fcodPostal.value, fciudad.value, fprovincia.value)
 
-    const tarjetasHtml = padron.reduce((acc, elemento, i) => {   
-
-        return acc = acc + `    
-            <div class="tarjeta"> 
-                <h3>${elemento.apellido} ${elemento.nombre}</h3>
-                <p>
-                    DNI: ${elemento.dni} <br>
-                    Domicilio: ${elemento.domicilio}<br>
-                    Cod. Postal: ${elemento.codPostal}<br>
-                    Ciudad: ${elemento.ciudad}<br>
-                    Privincia: ${elemento.provincia}<br>
-                </p> 
-            </div>
-        `       
-    },"")
-    
-    const contenedorAfiliados = document.querySelector(".contenedorAfiliados")
-    
-    contenedorAfiliados.innerHTML = tarjetasHtml
+    mostrarPadron()
+    formAltas.reset()
 
 }
+
+borrar.onclick = () => {
+    padron.pop();
+    alert("El registro ha sido borrado!")
+    subirPadronLS()
+    mostrarPadron()
+}
+
+
+logOut.onclick = () => {
+    location.reload()
+}
+
